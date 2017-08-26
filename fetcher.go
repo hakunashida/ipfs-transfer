a0ee305d6ac704eabe08808fb9afa2a485289fc1
hakunashida <hakuna.shida@protonmail.com>
2017-08-26 13:08:22 +0300

    break out fetcher

---
+++ fetcher.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/PuerkitoBio/fetchbot"
+	"github.com/PuerkitoBio/goquery"
+)
+
+var (
+	// Protect access to dup
+	mu sync.Mutex
+	// Duplicates table
+	dup = map[string]bool{}
+
+	// Command-line flags
+	seed        = flag.String("seed", "https://ultimate-guitar.com", "seed URL")
+	cancelAfter = flag.Duration("cancelafter", 0, "automatically cancel the fetchbot after a given time")
+	cancelAtURL = flag.String("cancelat", "", "automatically cancel the fetchbot at a given URL")
+	stopAfter   = flag.Duration("stopafter", 0, "automatically stop the fetchbot after a given time")
+	stopAtURL   = flag.String("stopat", "", "automatically stop the fetchbot at a given URL")
+	memStats    = flag.Duration("memstats", 0, "display memory statistics at a given interval")
+)
+
+func beginFetching() {
+	
+	fmt.Print("begin fetching")
+
+	// Parse the string defined in the configuration
+	parsedUrl, err := url.Parse(*seed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the muxer
+	mux := fetchbot.NewMux()
+
+	// Handle all errors the same
+	mux.HandleErrors(fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
+		fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
+	}))
+
+	// Handle GET requests for html responses, to parse the body and enqueue all links as HEAD
+	// requests.
+	mux.Response().Method("GET").ContentType("text/html").Handler(fetchbot.HandlerFunc(
+		func(ctx *fetchbot.Context, res *http.Response, err error) {
+			// Process the body to find the links
+			// TODO: Move this to a separate file
+			doc, err := goquery.NewDocumentFromResponse(res)
+			if err != nil {
+				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
+				return
+			}
+			// Enqueue all links as HEAD requests
+			enqueueLinks(ctx, doc)
+		}))
+
+	// Handle HEAD requests for html responses coming from the source host - we don't want
+	// to crawl links from other hosts.
+	mux.Response().Method("HEAD").Host(parsedUrl.Host).ContentType("text/html").Handler(fetchbot.HandlerFunc(
+		func(ctx *fetchbot.Context, res *http.Response, err error) {
+			if _, err := ctx.Q.SendStringGet(ctx.Cmd.URL().String()); err != nil {
+				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
+			}
+		}))
+
+	// Create the Fetcher, handle the logging first, then dispatch to the Muxer
+	h := logHandler(mux)
+	if *stopAtURL != "" || *cancelAtURL != "" {
+		stopURL := *stopAtURL
+		if *cancelAtURL != "" {
+			stopURL = *cancelAtURL
+		}
+		h = stopHandler(stopURL, *cancelAtURL != "", logHandler(mux))
+	}
+	f := fetchbot.New(h)
+
+	// First mem stat print must be right after creating the fetchbot
+	if *memStats > 0 {
+		// Print starting stats
+		printMemStats(nil)
+		// Run at regular intervals
+		runMemStats(f, *memStats)
+		// On exit, print ending stats after a GC
+		defer func() {
+			runtime.GC()
+			printMemStats(nil)
+		}()
+	}
+
+	// Start processing
+	q := f.Start()
+
+	// if a stop or cancel is requested after some duration, launch the goroutine
+	// that will stop or cancel.
+	if *stopAfter > 0 || *cancelAfter > 0 {
+		after := *stopAfter
+		stopFunc := q.Close
+		if *cancelAfter != 0 {
+			after = *cancelAfter
+			stopFunc = q.Cancel
+		}
+
+		go func() {
+			c := time.After(after)
+			<-c
+			stopFunc()
+		}()
+	}
+
+	// Enqueue the seed, which is the first entry in the dup map
+	dup[*seed] = true
+	_, err = q.SendStringGet(*seed)
+	if err != nil {
+		fmt.Printf("[ERR] GET %s - %s\n", *seed, err)
+	}
+	q.Block()
+}
--- ipfstransfer.go
+++ ipfstransfer.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
+	// "net/url"
 	"runtime"
 	"strings"
 	"sync"
@@ -62,7 +62,7 @@ func ExampleScrape() {
 	})
 }
 
-var (
+/*var (
 	// Protect access to dup
 	mu sync.Mutex
 	// Duplicates table
@@ -75,9 +75,13 @@ var (
 	stopAfter   = flag.Duration("stopafter", 0, "automatically stop the fetchbot after a given time")
 	stopAtURL   = flag.String("stopat", "", "automatically stop the fetchbot at a given URL")
 	memStats    = flag.Duration("memstats", 0, "display memory statistics at a given interval")
-)
+)*/
 
 func main() {
+
+	beginFetching()
+
+	fmt.Printf("ipfs-transfer started")
 
 	// 1. bind to port
 	gojiMux := goji.NewMux()
@@ -88,96 +92,6 @@ func main() {
 	// ExampleScrape()
 
 	flag.Parse()
-
-	// 3. start crawling
-	// Parse the provided seed
-	u, err := url.Parse(*seed)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create the muxer
-	mux := fetchbot.NewMux()
-
-	// Handle all errors the same
-	mux.HandleErrors(fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
-		fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
-	}))
-
-	// Handle GET requests for html responses, to parse the body and enqueue all links as HEAD
-	// requests.
-	mux.Response().Method("GET").ContentType("text/html").Handler(fetchbot.HandlerFunc(
-		func(ctx *fetchbot.Context, res *http.Response, err error) {
-			// Process the body to find the links
-			doc, err := goquery.NewDocumentFromResponse(res)
-			if err != nil {
-				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
-				return
-			}
-			// Enqueue all links as HEAD requests
-			enqueueLinks(ctx, doc)
-		}))
-
-	// Handle HEAD requests for html responses coming from the source host - we don't want
-	// to crawl links from other hosts.
-	mux.Response().Method("HEAD").Host(u.Host).ContentType("text/html").Handler(fetchbot.HandlerFunc(
-		func(ctx *fetchbot.Context, res *http.Response, err error) {
-			if _, err := ctx.Q.SendStringGet(ctx.Cmd.URL().String()); err != nil {
-				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
-			}
-		}))
-
-	// Create the Fetcher, handle the logging first, then dispatch to the Muxer
-	h := logHandler(mux)
-	if *stopAtURL != "" || *cancelAtURL != "" {
-		stopURL := *stopAtURL
-		if *cancelAtURL != "" {
-			stopURL = *cancelAtURL
-		}
-		h = stopHandler(stopURL, *cancelAtURL != "", logHandler(mux))
-	}
-	f := fetchbot.New(h)
-
-	// First mem stat print must be right after creating the fetchbot
-	if *memStats > 0 {
-		// Print starting stats
-		printMemStats(nil)
-		// Run at regular intervals
-		runMemStats(f, *memStats)
-		// On exit, print ending stats after a GC
-		defer func() {
-			runtime.GC()
-			printMemStats(nil)
-		}()
-	}
-
-	// Start processing
-	q := f.Start()
-
-	// if a stop or cancel is requested after some duration, launch the goroutine
-	// that will stop or cancel.
-	if *stopAfter > 0 || *cancelAfter > 0 {
-		after := *stopAfter
-		stopFunc := q.Close
-		if *cancelAfter != 0 {
-			after = *cancelAfter
-			stopFunc = q.Cancel
-		}
-
-		go func() {
-			c := time.After(after)
-			<-c
-			stopFunc()
-		}()
-	}
-
-	// Enqueue the seed, which is the first entry in the dup map
-	dup[*seed] = true
-	_, err = q.SendStringGet(*seed)
-	if err != nil {
-		fmt.Printf("[ERR] GET %s - %s\n", *seed, err)
-	}
-	q.Block()
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
