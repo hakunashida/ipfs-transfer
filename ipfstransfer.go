4ddfa2a87e5787d95c9d99c04a259901215682a6
hakunashida <hakunashida@protonmail.com>
2017-09-03 18:25:14 +0300

    add search ability

--- ipfstransfer.go
+++ ipfstransfer.go
@@ -63,12 +63,20 @@ func main() {
 	// 1. bind to port
 	gojiMux := goji.NewMux()
 	gojiMux.HandleFunc(pat.Get("/hello/:name"), hello)
+	gojiMux.HandleFunc(pat.Get("/search/:terms"), search)
 	http.ListenAndServe("localhost:8000", gojiMux)
 
 	defer disconnectDb()
 
 	// 2. run scrape example
 	// ExampleScrape()
+}
+
+func search(w http.ResponseWriter, r *http.Request) {
+	terms := pat.Param(r, "terms")
+	refs := searchDb(terms)
+	fmt.Fprintf(w, "Searching for: %s\n", terms)
+	fmt.Fprintf(w, "found:\n%s", refs)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
